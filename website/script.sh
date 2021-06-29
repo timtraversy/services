bb90273bf84533996ade85997fd55c92f13da680
Tim Traversy <timtrav@gmail.com>
2021-06-29 13:44:19 -0400

    Make docker script general

--- website/script.sh
+++ website/script.sh
@@ -4,4 +4,4 @@
 #     -p 1313:1313 \
 #     klakegg/hugo:ext-alpine \
 #     shell
-docker-compose run --user $(id -u):$(id -g) --service-ports server
+docker-compose run --user $(id -u):$(id -g) --service-ports server $@
